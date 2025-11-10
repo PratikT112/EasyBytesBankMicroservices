7a47f5a43b60c2e4bf3662dd6704fb9e4bd3ffe4
PratikT112 <pratiktelang112@gmail.com>
2025-11-11 00:14:18 +0530

    Commit upto auditor

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,5 +1,7 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber cannot be a null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be a null or empty")
     private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be a null or empty")
     private String branchAddress;
 }
--- accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,11 +1,25 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+
+    @NotEmpty(message = "Name cannot be null or empty")
+    @Size(min = 5, max = 40, message =  "The length of customer name should be between 5 and 40 characters")
     private String name;
+
+    @NotEmpty(message = "Email cannot be null or empty")
+    @Email(message = "Email address should be of valid format")
     private String email;
+
+    @NotEmpty
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be of 10 digits")
     private String mobileNumber;
+
     private AccountsDto accountsDto;
 }
