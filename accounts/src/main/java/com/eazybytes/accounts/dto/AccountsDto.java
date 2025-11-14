c0349fd8050ff2375869db01c293e7ad4d426a53
PratikT112 <pratiktelang112@gmail.com>
2025-11-15 00:16:56 +0530

    API-DOC-INIT

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,23 +1,33 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountsDto {
 
+    @Schema(
+            description = "Account number of Eazy Bank Account"
+    )
     @NotEmpty(message = "AccountNumber cannot be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @Schema(description = "Account type of Eazy Bank Account", example = "savings")
     @NotEmpty(message = "AccountType cannot be a null or empty")
     private String accountType;
 
+    @Schema(description = "Branch Address of Eazy Bank Branch", example = "savings")
     @NotEmpty(message = "BranchAddress cannot be a null or empty")
     private String branchAddress;
 }
--- accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -7,19 +8,27 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
+    @Schema(description = "Name of the customer", example = "Eazy Bytes")
     @NotEmpty(message = "Name cannot be null or empty")
     @Size(min = 5, max = 40, message =  "The length of customer name should be between 5 and 40 characters")
     private String name;
 
+    @Schema(description = "Email of the customer", example = "EazyBytes@mgail.com")
     @NotEmpty(message = "Email cannot be null or empty")
     @Email(message = "Email address should be of valid format")
     private String email;
 
+    @Schema(description = "Mobile number of the customer", example = "9472938473")
     @NotEmpty
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be of 10 digits")
     private String mobileNumber;
 
+    @Schema(description = "Account details of the customer")
     private AccountsDto accountsDto;
 }
